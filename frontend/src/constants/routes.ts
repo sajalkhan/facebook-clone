bf423620bdf4560109054ec957f3d7f6b995fa0b
sajalkhan <sajalhossen@yahoo.com>
2023-02-09 19:29:32 +0600

    remove unused page

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -1,5 +1,4 @@
 export const ROUTES = {
   HOME: '/home/',
   LOGIN: '/login/',
-  REGISTER: '/register/'
 } as const;
--- frontend/src/pages/routes.tsx
+++ frontend/src/pages/routes.tsx
@@ -5,7 +5,6 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('pages/home'));
 const Login = lazy(() => import('pages/login'));
-const Register = lazy(() => import('pages/register'));
 // const NotFound = lazy(() => import('pages/not-found'));
 
 export const PagesRoutes: React.FC = () => (
@@ -13,7 +12,6 @@ export const PagesRoutes: React.FC = () => (
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
       <Route path={ROUTES.LOGIN} element={<Login />} />
-      <Route path={ROUTES.REGISTER} element={<Register />} />
       <Route path="/" element={<Navigate to={ROUTES.LOGIN} />} />
       {/* <Route component={<NotFound />} /> */}
     </Routes>
