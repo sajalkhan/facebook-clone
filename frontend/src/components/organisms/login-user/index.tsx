fc19d8a557aa117f93f04bd1b495a2b14959e79b
sajalkhan <sajalhossen@yahoo.com>
2023-02-16 21:43:06 +0600

    update logo size

--- frontend/src/components/organisms/login-user/index.tsx
+++ frontend/src/components/organisms/login-user/index.tsx
@@ -10,12 +10,12 @@ type LoginUserProps = {
 
 export const LoginUser: React.FC<LoginUserProps> = ({ handleRegisterForm, handleSubmit }) => {
   return (
-    <div className="m-login-user">
-      <div className="m-login-user__container">
-        <div className="m-login-user__icon">
+    <div className="o-login-user">
+      <div className="o-login-user__container">
+        <div className="o-login-user__icon">
           <Icon iconName="facebook" />
         </div>
-        <span className="m-login-user__text">Facebook helps you connect and share with the people in your life.</span>
+        <span className="o-login-user__text">Facebook helps you connect and share with the people in your life.</span>
       </div>
       {/* login form */}
       <LoginForm handleRegisterForm={handleRegisterForm} handleSubmit={handleSubmit} />
