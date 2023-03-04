2f86bacaa20bfa764bf36ffc66cef88edaff60ef
sajalkhan <sajalhossen@yahoo.com>
2023-03-04 22:22:32 +0600

    update email validation schema

--- frontend/src/components/molecules/search-account-form/validation-schema.ts
+++ frontend/src/components/molecules/search-account-form/validation-schema.ts
@@ -1,5 +1,8 @@
 import * as Yup from 'yup';
 
 export const searchAccountValidation = Yup.object({
-  email: Yup.string().required('Email address is required.').email('Must be a valid email.').max(100)
+  email: Yup.string()
+    .required('Email address ir required.')
+    .email('Must be a valid email address.')
+    .max(50, "Email address can't be more than 50 characters.")
 });
