703f34c0214b905e7bf036197655f908babbc574
sajalkhan <sajalhossen@yahoo.com>
2023-04-10 15:48:35 +0600

    refactor changes

--- backend/src/helpers/awsS3.ts
+++ backend/src/helpers/awsS3.ts
@@ -1,7 +1,7 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import crypto from 'crypto';
 import { UploadedFile } from 'express-fileupload';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 const s3 = new S3Client({
   region: process.env.BUCKET_REGION,
